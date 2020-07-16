d1751268d3ab7d7da96354c2cbfbfc76fca36fa3
Ashish Yadav <iloveh4ck1ng@gmail.com>
2020-07-17 01:09:56 +0545

    first shot on dnd

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import "./styles.css";
-console.log("Hello");
 
-const drag = e => {};
+let dragRef = null;
 
-const drop = e => {};
+const drag = e => {
+  dragRef = e.target;
+  e.dataTransfer.setData("text/html", null);
+};
+
+const drop = e => {
+  e.preventDefault();
+  dragRef.parentNode.removeChild(dragRef);
+  e.target.appendChild(dragRef);
+};
+
+const dragover = e => e.preventDefault();
 
 let items = document.querySelectorAll(".draggable-items div");
-
-items.forEach(element => {
-  element.addEventListener("dragstart", drag);
+let containers = document.querySelectorAll(".container div");
+containers.forEach(element => {
   element.addEventListener("drop", drop);
+  element.addEventListener("dragover", dragover);
+});
+items.forEach(element => {
+  element.setAttribute("draggable", true);
+  element.addEventListener("dragstart", drag);
 });
